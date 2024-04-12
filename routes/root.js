e09de3d446680ffeceae0aab7376c562fb7854e5
The Legendary John <legendaryjohn2005@gmail.com>
2024-04-12 23:47:39 +0700

    Init template for landing page

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,10 @@
+import { characters } from "../data/characters.js";
 import { app } from "../app.js";
 
 app.get("/", (req, res) => {
 	res.render("index", {
-		title: "Hosme",
+		title: "Home",
 		message: "Welcome to Kessoku Hub!!",
+		characters: characters,
 	});
 });
