a5780a280d040dece37afea0ecaad5a8ab25b6aa
The Legendary John <legendaryjohn2005@gmail.com>
2024-04-18 00:25:26 +0700

    Implemented sidebar and hamburger menu

---
+++ public/js/sidebar.js
@@ -0,0 +1,39 @@
+let navbarClosed = true;
+
+const openAnim = () => {
+	document.querySelector("#start").beginElement();
+	navbarClosed = false;
+	console.log("Open animation");
+};
+
+const closeAnim = () => {
+	document.querySelector("#reverse").beginElement();
+	navbarClosed = true;
+	console.log("Close animation");
+};
+
+const toggleNav = () => {
+	console.log("Toggle navbar");
+	document.querySelector("#sidebar").classList.toggle("toggleSidebar");
+
+	navbarClosed ? openAnim() : closeAnim();
+};
+
+const closeNav = () => {
+	console.log("Close navbar");
+	document.querySelector("#sidebar").classList.remove("toggleSidebar");
+
+	if (!navbarClosed) {
+		closeAnim();
+	}
+};
+
+const detectIgnoredClass = (event) => {
+	event.stopPropagation();
+};
+
+document.querySelector("#menu-btn").addEventListener("click", toggleNav);
+document.querySelector("body").addEventListener("click", closeNav);
+document.querySelectorAll(".ignoreSidebarToggle").forEach((e) => {
+	e.addEventListener("click", detectIgnoredClass);
+});
