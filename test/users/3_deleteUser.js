16e69adac787dad192664d840ea0cc4fe58570de
Azza <azgamedeveloper@gmail.com>
2024-04-19 17:27:28 +0700

    removed delete user before hook

--- test/users/3_deleteUser.js
+++ test/users/3_deleteUser.js
@@ -2,20 +2,6 @@ import { User } from "../../schemas/user.js";
 import * as colors from "../../lib/consoleThemes.js";
 import assert from "assert";
 
-// This will run before running the test
-let user;
-before(function (done) {
-	// Creating a new Instance of User Model
-	user = new User({
-		username: "testuser",
-		password: "testpassword",
-	});
-	user.save().then(() => {
-		console.log(`${colors.test} User created`);
-		done();
-	});
-});
-
 describe(`${colors.test} Deleting a document from MongoDB`, () => {
 	it(`${colors.test} should delete a user`, (done) => {
 		User.findOneAndDelete({ username: "testuser" })
