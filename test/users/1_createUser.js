2d24d4f16024ea6a507a0ac8c50608212f1ca702
Azza <azgamedeveloper@gmail.com>
2024-04-19 19:30:35 +0700

    now flush collection inside the test

--- test/users/1_createUser.js
+++ test/users/1_createUser.js
@@ -3,6 +3,14 @@ import assert from "assert";
 
 // Describe the tests
 describe(`Creating a document in MongoDB`, function () {
+	// Run this hook before each tests
+	beforeEach(function (done) {
+		// Drop the users collection
+		User.collection.drop(() => {
+			done();
+		});
+	});
+
 	// Create a document
 	it(`should create a new user`, function (done) {
 		// Create a new user
--- test/users/2_readUser.js
+++ test/users/2_readUser.js
@@ -1,20 +1,20 @@
 import { User } from "../../schemas/user.js";
-import * as colors from "../../lib/consoleThemes.js";
 import assert from "assert";
 
 describe(`Reading a user's details from MongoDB`, () => {
 	// Create a new user to test
 	let user;
 	// This will run before running the test
-	before(function (done) {
-		// Creating a new Instance of User Model
-		user = new User({
-			username: "testuser",
-			password: "testpassword",
-		});
-		user.save().then(() => {
-			//console.log(`${colors.test} User created`);
-			done();
+	beforeEach(function (done) {
+		User.collection.drop(() => {
+			// Creating a new Instance of User Model
+			user = new User({
+				username: "testuser",
+				password: "testpassword",
+			});
+			user.save().then(() => {
+				done();
+			});
 		});
 	});
 
