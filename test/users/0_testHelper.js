e004790cbd2848d65b347db56a8e09adfd88c6e8
Azza <azgamedeveloper@gmail.com>
2024-04-19 19:30:07 +0700

    removed hook and added before

--- test/users/0_testHelper.js
+++ test/users/0_testHelper.js
@@ -9,25 +9,21 @@ if (!process.env.DB_PASSWORD)
 		`${colors.db} ${colors.error} DB_PASSWORD not found in .env file! If you're part of the Kessoku team, please ask for the password.`
 	);
 // Connect to the test database
-mongoose
-	.connect(
-		`mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.nejuznn.mongodb.net/test?retryWrites=true&w=majority`
-	)
-	.then(() =>
-		console.log(`${colors.test} ${colors.db} Connected to testing MongoDB!`)
-	)
-	.catch((err) =>
-		console.error(
-			`${colors.db} ${colors.error} Could not connect to MongoDB:`,
-			err.message
+before(function (done) {
+	mongoose
+		.connect(
+			`mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.nejuznn.mongodb.net/test?retryWrites=true&w=majority`
 		)
-	);
-
-// Run this hook before each tests
-beforeEach(function (done) {
-	// Drop the users collection
-	mongoose.connection.collections.users.drop(() => {
-		// console.log(`${colors.test} ${colors.db} Dropped the users collection`);
-		done();
-	});
+		.then(() => {
+			console.log(
+				`${colors.test} ${colors.db} Connected to testing MongoDB!`
+			);
+			done();
+		})
+		.catch((err) =>
+			console.error(
+				`${colors.db} ${colors.error} Could not connect to MongoDB:`,
+				err.message
+			)
+		);
 });
