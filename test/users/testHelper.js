b26470af6e1afed771c2e625c2ed9f43b4b8a63a
Azza <azgamedeveloper@gmail.com>
2024-04-19 13:46:27 +0700

    added a pretest hook

---
+++ test/users/testHelper.js
@@ -0,0 +1,19 @@
+import { mongoose } from "../../lib/db.js";
+import colors from "../../lib/consoleThemes.js";
+
+// Flushes the database before running tests
+mongoose.connection
+	.once("open", () => {
+		mongoose.connection.db.dropDatabase();
+		console.log(`${colors.test} ${colors.db} Database flushed!`);
+	})
+	.on("error", (error) => {
+		console.warn(`${colors.test} ${colors.db} ${colors.warn} `, error);
+	});
+
+// Run this hook before each tests
+beforeEach((done) => {
+	mongoose.connection.collections.users.drop(() => {
+		done();
+	});
+});
