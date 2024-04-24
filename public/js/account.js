8a51314972c30dcdbdf3047c387b0a727103b829
AzDeveloper <azgamedeveloper@gmail.com>
2024-04-25 02:57:43 +0700

    added signup form functionality

--- public/js/account.js
+++ public/js/account.js
@@ -28,3 +28,89 @@ document.querySelector("#signup").addEventListener("click", toggleSections);
 document.querySelector("#login").addEventListener("click", toggleSections);
 
 // Listen to the form submission
+document
+	.querySelector("#login-form")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
+		// Create a formdata object
+		let formData = new FormData(event.target);
+		let response = await fetch("/user/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: formData.get("username"),
+				password: formData.get("password"),
+			}),
+		});
+
+		if (response.status === 200) {
+			window.location.href =
+				"/account/success?username=" + formData.get("username");
+		} else {
+			let alert = document.querySelector("#error-login-alert");
+			alert.style.opacity = "1";
+			alert.textContent = "Invalid username/password.";
+			setTimeout(() => {
+				alert.style.opacity = "0";
+			}, 3000);
+		}
+	});
+
+// Register form
+document
+	.querySelector("#signup-form")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
+		// Create a formdata object
+		let formData = new FormData(event.target);
+		// Verify whether confirm password matches password
+		if (formData.get("password") !== formData.get("confirm-password")) {
+			let alert = document.querySelector("#error-signup-alert");
+			alert.textContent = "Passwords do not match.";
+			alert.style.opacity = "1";
+			setTimeout(() => {
+				alert.style.opacity = "0";
+			}, 3000);
+			return false;
+		}
+		// Send the request
+		let response = await fetch("/user/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: formData.get("username"),
+				password: formData.get("password"),
+			}),
+		});
+
+		if (response.status === 201) {
+			window.location.href =
+				"/account/success?username=" + formData.get("username");
+		} else {
+			let error = await response.json();
+			// Handle individual errors
+			if (response.status === 400) {
+				error.forEach((e) => {
+					let alert = document.querySelector(
+						`#${e.path[0]}-signup-alert`
+					);
+					alert.textContent = e.message;
+					alert.style.opacity = "1";
+					setTimeout(() => {
+						alert.style.opacity = "0";
+					}, 3000);
+				});
+			} else {
+				let alert = document.querySelector("#error-signup-alert");
+				alert.textContent = error;
+				alert.style.opacity = "1";
+				setTimeout(() => {
+					alert.style.opacity = "0";
+				}, 3000);
+			}
+		}
+	});
