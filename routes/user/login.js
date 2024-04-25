13f89ffc12d96821887c04c83cf7e1380e7e895e
AzDeveloper <azgamedeveloper@gmail.com>
2024-04-25 14:59:06 +0700

    updated import path

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { User } from "../../schemas/user.js";
+import { User } from "../../models/user.js";
 import { Validator } from "jsonschema";
 //
 const router = Router();
