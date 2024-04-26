eac7e5f8de7f9c78525b7cfecf177ed4db991306
Azza <azgamedeveloper@gmail.com>
2024-04-26 12:58:04 +0700

    logout route functionality

--- routes/user/logout.js
+++ routes/user/logout.js
@@ -2,16 +2,20 @@ import { Router } from "express";
 //
 const router = Router();
 
-router.get("/user/logout", (req, res, next) => {
+export function logout(req, res, next) {
 	req.session.user = null;
 	req.session.save((err) => {
 		if (err) next(err);
 
 		req.session.regenerate((err) => {
 			if (err) next(err);
-			res.status(200).json("Successfully logged out.");
 		});
 	});
+}
+
+router.get("/user/logout", (req, res, next) => {
+	logout(req, res, next);
+	res.status(200).json("Successfully logged out.");
 });
 
 export default router;
