c890fea958adddbe11a4ffe26ba6ca0a8d914533
The Legendary John <legendaryjohn2005@gmail.com>
2024-04-29 01:02:10 +0700

    Moved size selection script in "store-section" to a dedicated file

---
+++ public/js/landing/store-section/sizeSelection.js
@@ -0,0 +1,17 @@
+const sizeBtn = document.querySelectorAll(".size-select");
+let selectedSize;
+
+sizeBtn.forEach((e) => {
+	e.addEventListener("click", (e) => {
+		// Remove all "selected" classes in the same parent
+		e.target.parentNode.querySelectorAll(".selected").forEach((e) => {
+			e.classList.remove("selected");
+		});
+
+		// Add "selected" class to clicked button
+		e.target.classList.add("selected");
+
+		// Set selected size
+		selectedSize = e.target.textContent;
+	});
+});
