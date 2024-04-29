bb4af54e70ac581dc1cb65f0fe1b25df4dc5f8ca
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-29 14:23:02 +0700

    initial pop-up design

--- routes/store/store.js
+++ routes/store/store.js
@@ -18,6 +18,54 @@ let items = [
 		price: 30.0,
 		stock: 5,
 	},
+	{
+		id: 1, // Should be a unique identifier, randomly generated. Take _id from the database
+		imageName: "bocchi-sweater.png",
+		name: "Bocchi's Face T-Shirt",
+		description: "A T-shirt with Bocchi's face on it.",
+		price: 20.0,
+		stock: 10,
+	},
+	{
+		id: 2,
+		imageName: "kessoku-tshirt.png ",
+		name: "Kessoku Hoodie",
+		description: "A hoodie with the Kessoku logo.",
+		price: 30.0,
+		stock: 5,
+	},
+	{
+		id: 1, // Should be a unique identifier, randomly generated. Take _id from the database
+		imageName: "bocchi-sweater.png",
+		name: "Bocchi's Face T-Shirt",
+		description: "A T-shirt with Bocchi's face on it.",
+		price: 20.0,
+		stock: 10,
+	},
+	{
+		id: 2,
+		imageName: "kessoku-tshirt.png ",
+		name: "Kessoku Hoodie",
+		description: "A hoodie with the Kessoku logo.",
+		price: 30.0,
+		stock: 5,
+	},
+	{
+		id: 1, // Should be a unique identifier, randomly generated. Take _id from the database
+		imageName: "bocchi-sweater.png",
+		name: "Bocchi's Face T-Shirt",
+		description: "A T-shirt with Bocchi's face on it.",
+		price: 20.0,
+		stock: 10,
+	},
+	{
+		id: 2,
+		imageName: "kessoku-tshirt.png ",
+		name: "Kessoku Hoodie",
+		description: "A hoodie with the Kessoku logo.",
+		price: 30.0,
+		stock: 5,
+	},
 ];
 //
 const router = Router();
