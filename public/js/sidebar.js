601b0b7ae4fe25edd6f254aca32e59ffa3188f74
The Legendary John <legendaryjohn2005@gmail.com>
2024-05-03 10:44:02 +0700

    Removed old HTML console logs

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -5,7 +5,7 @@ const openAnim = () => {
 	document.querySelector("#menu-btn").classList.add("btnToggle");
 	document.querySelector("#menu-btn-path").attributes[1].value = "#000000";
 	navbarClosed = false;
-	console.log("Open animation");
+	// console.log("Open animation");
 };
 
 const closeAnim = () => {
@@ -13,11 +13,11 @@ const closeAnim = () => {
 	document.querySelector("#menu-btn").classList.remove("btnToggle");
 	document.querySelector("#menu-btn-path").attributes[1].value = "#C45AFF";
 	navbarClosed = true;
-	console.log("Close animation");
+	// console.log("Close animation");
 };
 
 const toggleNav = () => {
-	console.log("Toggle navbar");
+	// console.log("Toggle navbar");
 	document.querySelector("#sidebar").classList.toggle("toggleSidebar");
 	// document.querySelector("#menu-btn").classList.toggle("btnToggle");
 
@@ -25,7 +25,7 @@ const toggleNav = () => {
 };
 
 const closeNav = () => {
-	console.log("Close navbar");
+	// console.log("Close navbar");
 	document.querySelector("#sidebar").classList.remove("toggleSidebar");
 
 	if (!navbarClosed) {
