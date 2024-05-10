593a284840d6aafe55a058b3726b3bbd5dceb650
thelegendaryjohn <legendaryjohn2005@gmail.com>
2024-05-10 15:28:59 +0700

    Changed the route to accountEdit

--- routes/account/account.js
+++ routes/account/account.js
@@ -18,6 +18,10 @@ router.get("/account/menu", (req, res) => {
 	render(req, res, "account/accountMenu");
 });
 
+router.get("/account/preferences/profile", (req, res) => {
+	render(req, res, "account/accountEdit");
+})
+
 router.get("/account/logout", (req, res, next) => {
 	// Logs the user out
 	logout(req, res, next);
