58285599aa746118a96704e5271e9afb59899446
thelegendaryjohn <legendaryjohn2005@gmail.com>
2024-05-13 19:36:30 +0700

    Basic forum fetching from JS file (unfinished)

--- routes/forum/forum.js
+++ routes/forum/forum.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { render } from "../../lib/render.js";
+import { thread } from "../../public/js/forum/forum.js";
 //
 const router = Router();
 
 router.get("/forum", (req, res) => {
-	render(req, res, "forum/forumPage");
+	render(req, res, "forum/forumPage", {
+		thread: thread,
+	});
 });
 
 router.get("/forum/thread", (req, res) => {
