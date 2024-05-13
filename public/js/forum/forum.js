663ba22958495bc0715e2581b7e6436ebfc587ce
thelegendaryjohn <legendaryjohn2005@gmail.com>
2024-05-13 19:36:30 +0700

    Basic forum fetching from JS file (unfinished)

---
+++ public/js/forum/forum.js
@@ -0,0 +1,117 @@
+export let thread = [
+	// Thread 0
+	{
+		threadTitle: "Walkman Collaboration",
+		threadStatus: {
+			views: 3919,
+			comments: 164,
+			likes: 1174,
+		},
+		threadPosts: [
+			// Post 0
+			{
+				postAuthor: "Hitori Gotoh",
+				postTitle:
+					"New Collab Drop: Walkman x Tokyo Threads - Get Yours Now!",
+				postStatus: {
+					views: 123,
+					comments: 12,
+					likes: 35,
+				},
+			},
+
+			// Post 1
+			{
+				postAuthor: "Nijika Ijichi",
+				postTitle:
+					"Tokyo Tech: Walkman x Street Gear - Upgrade Your Style!",
+				postStatus: {
+					views: 1248,
+					comments: 45,
+					likes: 352,
+				},
+			},
+
+			// Post 2
+			{
+				postAuthor: "Ryo Yamada",
+				postTitle:
+					"Limited Edition: Walkman x Bocchi The Rock! - Rock 'n Roll!",
+				postStatus: {
+					views: 2015,
+					comments: 75,
+					likes: 642,
+				},
+			},
+
+			// Post 3
+			{
+				postAuthor: "Ikuyo Kita",
+				postTitle:
+					"Cross-country collab: Walkman x SHEIN - Don't Miss Out!",
+				postStatus: {
+					views: 533,
+					comments: 32,
+					likes: 145,
+				},
+			},
+		],
+	},
+
+	// Thread 1
+	{
+		threadTitle: "AniPlex Spring Fair",
+		threadStatus: {
+			views: 21912,
+			comments: 1378,
+			likes: 8967,
+		},
+		threadPosts: [
+			// Post 0
+			{
+				postAuthor: "Nijika Ijichi",
+				postTitle:
+					"Exclusive Sneak Peek: Limited Edition Figures Unveiled",
+				postStatus: {
+					views: 634,
+					comments: 54,
+					likes: 139,
+				},
+			},
+
+			// Post 1
+			{
+				postAuthor: "Ikuyo Kita",
+				postTitle:
+					"Cosplay Contest Announcement: Show off Your Best Anime-Inspired Outfits!",
+				postStatus: {
+					views: 5248,
+					comments: 274,
+					likes: 734,
+				},
+			},
+
+			// Post 2
+			{
+				postAuthor: "Hitori Gotoh",
+				postTitle: "Anime Merch: Limited Edition Figures Unveiled",
+				postStatus: {
+					views: 2015,
+					comments: 75,
+					likes: 642,
+				},
+			},
+
+			// Post 3
+			{
+				postAuthor: "Ryo Yamada",
+				postTitle: "Virtual Reality Experience: Step into the World of 'Sword Art Online'",
+                postStatus: {
+                    views: 14015,
+                    comments: 975,
+                    likes: 7452,
+                }
+			}
+		],
+	},
+];
