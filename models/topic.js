0afcde85faa8f93ff9c34996516e77e2bc411561
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-19 09:31:22 +0700

    added topic models

---
+++ models/topic.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// Define the schema
+const topicSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+		index: true,
+	},
+	description: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
+});
+
+// Export the model
+export const Topic = mongoose.model("Topic", topicSchema, "topics");
