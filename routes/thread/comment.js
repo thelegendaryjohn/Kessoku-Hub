749aaa4242dc84a9b17034bc8b111c2a80ce450c
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-19 13:14:41 +0700

    Add route for posting comments

---
+++ routes/thread/comment.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import { Comment } from "../../models/comment.js";
+import { Validator } from "jsonschema";
+//
+const router = Router();
+//
+const v = new Validator();
+let schema = {
+	type: "object",
+	properties: {
+		content: { type: "string" },
+		postId: { type: "string" },
+	},
+	required: ["content", "postId"],
+};
+
+// Posting a comment
+router.post("/thread/comment", (req, res) => {
+	// Validate the input
+	let result = v.validate(req.body, schema);
+	if (!result.valid) {
+		return res.status(401).json("Invalid input.");
+	}
+
+	// Check whether the user is logged in
+	if (!req.session.user) {
+		return res.status(401).json("Unauthorized.");
+	}
+
+	// Create a new comment
+	let comment = new Comment({
+		content: req.body.content,
+		authorId: req.session.user._id,
+		postId: req.body.postId,
+	});
+
+	comment
+		.save()
+		.then((comment) => {
+			return res.status(200).json(comment);
+		})
+		.catch((err) => {
+			return res.status(500).json(err);
+		});
+});
