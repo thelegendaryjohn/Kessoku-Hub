e99455f209277e67eb216b191ca34e7f989da22a
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-19 09:30:48 +0700

    added comment model

---
+++ models/comment.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+	postId: {
+		type: Schema.Types.ObjectId,
+		ref: "Post",
+		required: true,
+		index: true,
+	},
+	authorId: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+		index: true,
+	},
+	content: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
+	viewCount: { type: Number, default: 0 },
+	likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+// Export the model
+export const Comment = mongoose.model("Comment", commentSchema, "comments");
