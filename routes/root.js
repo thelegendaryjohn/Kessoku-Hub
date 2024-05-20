618543bc3cca3755003add4337c2c0a2a2293151
Azza <azgamedeveloper@gmail.com>
2024-05-20 15:27:42 +0700

    refactor: Update landing page forum section to display topics and posts

--- routes/root.js
+++ routes/root.js
@@ -1,13 +1,30 @@
 import { Router } from "express";
 import { render } from "../lib/render.js";
 import { characters } from "../public/js/landing/characters.js";
+import { Topic } from "../models/topic.js";
+import { Post } from "../models/post.js";
 //
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+	// Show announcement and discussion topics on the landing page
+	const topics = await Topic.find({
+		$or: [{ name: "Announcements" }, { name: "General Discussion" }],
+	});
+	let posts = {};
+	for (let topic of topics) {
+		posts[topic._id] = await Post.find({ topicId: topic._id })
+			.sort({ createdAt: -1 })
+			.limit(2)
+			.populate("author");
+	}
+
+	console.log("topics", topics);
+
 	render(req, res, "landingPage", {
 		charNames: characters.map((char) => char.id),
-		topic: [],
+		topics: topics,
+		posts: posts,
 	});
 });
 
