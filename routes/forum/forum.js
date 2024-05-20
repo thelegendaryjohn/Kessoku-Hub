f31fc43bb7384eb6c2bbce37113d9321491c6abf
Azza <azgamedeveloper@gmail.com>
2024-05-20 13:38:19 +0700

    renames and implementation

--- routes/forum/forum.js
+++ routes/forum/forum.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { render } from "../../lib/render.js";
+import { Topic } from "../../models/topic.js";
+import { Post } from "../../models/post.js";
 //
 const router = Router();
 
-router.get("/forum", (req, res) => {
-	render(req, res, "forum/forumPage", {});
+router.get("/forum", async (req, res) => {
+	const topics = await Topic.find({});
+	let posts = {};
+	for (let topic of topics) {
+		posts[topic._id] = await Post.find({ topicId: topic._id })
+			.sort({ createdAt: -1 })
+			.limit(3)
+			.populate("authorId");
+	}
+	render(req, res, "forum/forumPage", {
+		topics: topics,
+		posts: posts,
+	});
 });
 
 router.get("/forum/topic", (req, res) => {
