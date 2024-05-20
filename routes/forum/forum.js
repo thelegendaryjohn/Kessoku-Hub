0113c99f9368a5eb4e67ab65d957c5b312da64b0
Azza <azgamedeveloper@gmail.com>
2024-05-20 13:13:43 +0700

    renamed processing

--- routes/forum/forum.js
+++ routes/forum/forum.js
@@ -1,37 +1,26 @@
 import { Router } from "express";
 import { render } from "../../lib/render.js";
-import { thread } from "../../public/js/forum/forum.js";
 //
 const router = Router();
 
 router.get("/forum", (req, res) => {
-	render(req, res, "forum/forumPage", {
-		thread: thread,
-	});
+	render(req, res, "forum/forumPage", {});
 });
 
-router.get("/forum/thread", (req, res) => {
-	render(req, res, "forum/threadPage", {
-		thread: thread,
-	});
+router.get("/forum/topic", (req, res) => {
+	render(req, res, "forum/topicPage", {});
 });
 
 router.get("/forum/post", (req, res) => {
-	render(req, res, "forum/postPage", {
-		thread: thread,
-	});
-})
+	render(req, res, "forum/postPage", {});
+});
 
 router.get("/forum/post/create", (req, res) => {
-	render(req, res, "forum/postCreatePage", {
-		thread: thread,
-	});
+	render(req, res, "forum/postCreatePage", {});
 });
 
 router.get("/forum/inbox", (req, res) => {
-	render(req, res, "forum/notifPage", {
-		thread: thread,
-	});
+	render(req, res, "forum/notifPage", {});
 });
 
 export default router;
