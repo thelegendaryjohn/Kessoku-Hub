40db5822c8f99cc07557c66bde8aeb566c7022c4
Geno2806 <quockhanhmini@gmail.com>
2024-05-20 14:32:58 +0700

    Create way to import video links to videoPage

--- routes/video/video.js
+++ routes/video/video.js
@@ -3,8 +3,29 @@ import { render } from "../../lib/render.js";
 //
 const router = Router();
 
+let videos = [
+    {
+        id: "Yd8kUoB72xU",
+        name: "Seishun Complex - Music Video",
+    },
+    {
+        id: "Yd8kUoB72xU",
+        name: "Seishun Complex - Music Video",
+    },
+    {
+        id: "Yd8kUoB72xU",
+        name: "Seishun Complex - Music Video",
+    },
+    {
+        id: "q0tX10H8VGA",
+        name: "Ep. 9 PV - Enoshima Escar",
+    }
+	
+];
 router.get("/videos", (req, res) => {
-	render(req, res, "video/videoPage");
+	render(req, res, "video/videoPage", {
+		videos: videos,
+	});
 });
 
 export default router;
