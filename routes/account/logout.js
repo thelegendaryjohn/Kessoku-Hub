3e86c9d00bf091a6a901b2db68e2161dda4a4ff0
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-21 23:56:18 +0700

    logout and readme fixes

--- routes/account/logout.js
+++ routes/account/logout.js
@@ -2,25 +2,27 @@ import { Router } from "express";
 //
 const router = Router();
 
-export function logout(req, res, next) {
-	req.session.user = null;
-	req.session.save((err) => {
-		if (err) next(err);
-
-		req.session.regenerate((err) => {
-			if (err) next(err);
-		});
+export function logout(req, callback) {
+	req.session.destroy((err) => {
+		if (err) {
+			return callback(err);
+		}
+		return callback(null);
 	});
 }
 
-router.post("/account/logout", (req, res, next) => {
+router.post("/account/logout", (req, res) => {
 	// See if the user is logged in and the request is valid
 	if (!req.session.user) {
 		return res.status(401).json("Unauthorized.");
 	}
 
-	logout(req, res, next);
-	return res.status(200).json("Logged out.");
+	logout(req, (err) => {
+		if (err) {
+			return res.status(500).json("Error logging out.");
+		}
+		return res.status(200).json("Logged out.");
+	});
 });
 
 export default router;
