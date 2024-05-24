dae8b9884de1d06f83dfef81488823e3c9fa1a2a
Geno2806 <quockhanhmini@gmail.com>
2024-05-24 13:08:20 +0700

    Added video length property

--- routes/video/video.js
+++ routes/video/video.js
@@ -7,43 +7,45 @@ let videos = [
 	{
 		id: "5rWDFQ4PzOY",
 		name: "Ep. 1 PV - Lonely Rolling Bocchi",
-		videoType: "previews"
+		videoType: "previews",
+		length: "0:47"
 	},
 	{
 		id: "oI8vZB-SRBE",
 		name: "Ep. 2 PV - See You Tomorrow",
-		videoType: "previews"
+		videoType: "previews",
+		length: "0:49"
 	},
 	{
 		id: "WZxl3XhwCDQ",
 		name: "Ep. 3 PV - Be Right There",
-		videoType: "previews"
+		videoType: "previews",
+		length: "0:52"
 	},
 	{
 		id: "q0tX10H8VGA",
 		name: "Ep. 9 PV - Enoshima Escar",
-		videoType: "previews"
+		videoType: "previews",
+		length: "1:00"
 	},
     {
         id: "Yd8kUoB72xU",
         name: "Seishun Complex - Music Video",
-		videoType: "music"
+		videoType: "music",
+		length: "1:31"
     },
     {
         id: "Yd8kUoB72xU",
         name: "Seishun Complex - Music Video",
-		videoType: "music"
+		videoType: "music",
+		length: "1:31"
     },
     {
         id: "Yd8kUoB72xU",
         name: "Seishun Complex - Music Video",
-		videoType: "music"
+		videoType: "music",
+		length: "1:31"
     },
-    {
-        id: "Yd8kUoB72xU",
-        name: "Seishun Complex - Music Video",
-		videoType: "music"
-    },	
 ];
 router.get("/videos", (req, res) => {
 	render(req, res, "video/videoPage", {
