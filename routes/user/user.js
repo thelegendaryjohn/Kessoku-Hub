f91bcba55c611d81aedfd66543f12ef2a4f2f8ef
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-29 06:50:46 +0700

    added user API routing

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,7 +1,24 @@
 import { Router } from "express";
+import { User } from "../../models/user.model";
 //
 const router = Router();
 
-// Getting a user by ID
+// Getting a user by ID. This route is only meant for API usage.
+router.get("/:id", (req, res) => {
+	if (!req.params.id) {
+		return res.status(400).send("Missing URL parameter: ID");
+	}
+
+	User.findById(req.params.id, (err, user) => {
+		if (err) {
+			return res.status(500).json(err);
+		} else {
+			let userObj = user.toObject();
+			// Remove sensitive data
+			delete userObj.password;
+			return res.status(200).json(userObj);
+		}
+	});
+});
 
 export default router;
