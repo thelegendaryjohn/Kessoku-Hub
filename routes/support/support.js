7d5a90bebaf74b6b87bae01023c0caa0578d286c
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-29 04:44:34 +0700

    base route and post handler

---
+++ routes/support/support.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { render } from "../lib/render.js";
+//
+const router = Router();
+
+router.get("/support", async (req, res) => {
+	// Show the contact us page
+	render(req, res, "support", {});
+});
+
+// POST request to submit a contact form
+router.post("/support", async (req, res) => {
+	// Process the contact form
+	if (!req.body.name || !req.body.email || !req.body.message) {
+		return res.status(401).json("All fields are required.");
+	}
+
+	// Save the message to the database
+
+	res.redirect("/support");
+});
+
+export default router;
