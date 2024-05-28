bfc503b57fbbc08e5724823a8b839e408499f61b
Geno2806 <quockhanhmini@gmail.com>
2024-05-28 22:14:09 +0700

    Populate all available videos

--- routes/video/video.js
+++ routes/video/video.js
@@ -23,6 +23,36 @@ let videos = [
 		length: "0:52"
 	},
 	{
+		id: "QeeKTJhaJLo",
+		name: "Ep. 4 PV - Jumping Girls",
+		videoType: "previews",
+		length: "1:11"
+	},
+	{
+		id: "_m1VxtErpPo",
+		name: "Ep. 5 PV - Flightless Fish",
+		videoType: "previews",
+		length: "0:51"
+	},
+	{
+		id: "FM5ld8Tan4g",
+		name: "Ep. 6 PV - Eight Views",
+		videoType: "previews",
+		length: "0:46"
+	},
+	{
+		id: "Hi3dGsvgciI",
+		name: "Ep. 7 PV - To Your House",
+		videoType: "previews",
+		length: "1:04"
+	},
+	{
+		id: "IUO4neBlZI8",
+		name: "Ep. 8 PV - Bocchi The Rock",
+		videoType: "previews",
+		length: "0:40"
+	},
+	{
 		id: "q0tX10H8VGA",
 		name: "Ep. 9 PV - Enoshima Escar",
 		videoType: "previews",
@@ -35,16 +65,34 @@ let videos = [
 		length: "1:31"
     },
     {
-        id: "Yd8kUoB72xU",
-        name: "Seishun Complex - Music Video",
+        id: "B7BxrAAXl94",
+        name: "Guitar, Loneliness and Blue Planet - Music Video",
 		videoType: "music",
-		length: "1:31"
+		length: "3:53"
     },
     {
-        id: "Yd8kUoB72xU",
-        name: "Seishun Complex - Music Video",
+        id: "L2i0i9gWE00",
+        name: "That Band - Music Video",
 		videoType: "music",
-		length: "1:31"
+		length: "3:32"
+    },
+    {
+        id: "gX9m-rCtSqc",
+        name: "Never Forget - Music Video",
+		videoType: "music",
+		length: "3:37"
+    },
+    {
+        id: "wSTbdqo-j74",
+        name: "If I Could Be a Constellation - Music Video",
+		videoType: "music",
+		length: "4:14"
+    },
+    {
+        id: "29t3pJd75XU",
+        name: "Into the Light - Music Video",
+		videoType: "music",
+		length: "4:19"
     },
 ];
 router.get("/videos", (req, res) => {
