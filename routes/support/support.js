cb1cb3e9945ed03ed5aed3ac520bceb72c8a815d
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-31 00:35:29 +0700

    chore: Update support form route and model

--- routes/support/support.js
+++ routes/support/support.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { render } from "../lib/render.js";
+import { render } from "../../lib/render.js";
 import { SupportForm } from "../../models/supportForm.js";
 //
 const router = Router();
@@ -12,18 +12,27 @@ router.get("/support", async (req, res) => {
 // POST request to submit a contact form
 router.post("/support", async (req, res) => {
 	// Process the contact form
-	if (!req.body.name || !req.body.email || !req.body.message) {
+	if (!req.body.subject || !req.body.email || !req.body.message) {
 		return res.status(401).json("All fields are required.");
 	}
 
 	// Save the message to the database
-	await SupportForm.create({
+	let result = await SupportForm.create({
 		email: req.body.email,
-		name: req.body.name,
+		subject: req.body.subject,
 		message: req.body.message,
 	});
+	if (!result) {
+		return res.status(500).json("Failed to save message.");
+	} else {
+		return res.status(200).json("Message saved.");
+	}
+});
 
-	res.redirect("/support");
+router.get("/support/success", (req, res) => {
+	render(req, res, "account/accountSuccess", {
+		message: `Your message has been sent!`,
+	});
 });
 
 export default router;
