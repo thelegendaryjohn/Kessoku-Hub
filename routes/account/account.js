6bae1594cdcd58c8d1bb0adf496815ee5320c157
The Legendary John <legendaryjohn2005@gmail.com>
2024-05-31 03:27:23 +0700

    Created and styled accountPassForgot

--- routes/account/account.js
+++ routes/account/account.js
@@ -35,6 +35,10 @@ router.get("/account/settings", (req, res) => {
 	return render(req, res, "account/accountSettings");
 });
 
+router.get("/account/forgot-password", (req, res) => {
+	render(req, res, "account/accountPassForgot");
+});
+
 router.get("/account/logout", (req, res) => {
 	// See if the user is logged in and the request is valid
 	if (!req.session.user) {
