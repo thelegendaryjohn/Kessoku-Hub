0b7fc8b39eea8935c04960fc63bb577aec0a4e7a
AzDeveloper <azgamedeveloper@gmail.com>
2024-05-30 21:53:35 +0700

    chore: Update login and signup forms to include reCAPTCHA token

--- public/js/account.js
+++ public/js/account.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const signup = document.querySelector("#signup-section");
 const login = document.querySelector("#login-section");
 
@@ -65,24 +66,33 @@ document
 	.querySelector("#login-form")
 	.addEventListener("submit", async (event) => {
 		event.preventDefault();
-		// Create a formdata object
-		let formData = new FormData(event.target);
-		let response = await loginRequest({
-			username: formData.get("username"),
-			password: formData.get("password"),
-			remember: formData.get("remember") === "on" ? true : false,
-		});
+		grecaptcha.ready(function () {
+			grecaptcha
+				.execute("reCAPTCHA_site_key", { action: "submit" })
+				.then(async function (token) {
+					// Create a formdata object
+					let formData = new FormData(event.target);
+					let response = await loginRequest({
+						username: formData.get("username"),
+						password: formData.get("password"),
+						remember:
+							formData.get("remember") === "on" ? true : false,
+						token: token,
+					});
 
-		if (response.status === 200) {
-			window.location.href =
-				"/account/success?username=" + formData.get("username");
-		} else {
-			clearErrorHighlight();
-			errorAlert(
-				document.querySelector("#error-login-alert"),
-				"Invalid username/password."
-			);
-		}
+					if (response.status === 200) {
+						window.location.href =
+							"/account/success?username=" +
+							formData.get("username");
+					} else {
+						clearErrorHighlight();
+						errorAlert(
+							document.querySelector("#error-login-alert"),
+							"Invalid username/password."
+						);
+					}
+				});
+		});
 	});
 
 // Register form
@@ -90,71 +100,83 @@ document
 	.querySelector("#signup-form")
 	.addEventListener("submit", async (event) => {
 		event.preventDefault();
+		grecaptcha.ready(function () {
+			grecaptcha
+				.execute("reCAPTCHA_site_key", { action: "submit" })
+				.then(async function (token) {
+					// Create a formdata object
+					let formData = new FormData(event.target);
+					// Verify whether confirm password matches password
+					if (
+						formData.get("password") !==
+						formData.get("confirm-password")
+					) {
+						clearErrorHighlight();
+						errorAlert(
+							document.querySelector("#error-signup-alert"),
+							"Passwords do not match."
+						);
+						return false;
+					}
+					// Send the request
+					let response = await fetch("/account/register", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							username: formData.get("username"),
+							password: formData.get("password"),
+							token: token,
+						}),
+					});
 
-		// Create a formdata object
-		let formData = new FormData(event.target);
-		// Verify whether confirm password matches password
-		if (formData.get("password") !== formData.get("confirm-password")) {
-			clearErrorHighlight();
-			errorAlert(
-				document.querySelector("#error-signup-alert"),
-				"Passwords do not match."
-			);
-			return false;
-		}
-		// Send the request
-		let response = await fetch("/account/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				username: formData.get("username"),
-				password: formData.get("password"),
-			}),
-		});
+					if (response.status === 201) {
+						// If account was successfully created, additionally log the user in
+						let response = await loginRequest({
+							username: formData.get("username"),
+							password: formData.get("password"),
+							remember: true,
+						});
 
-		if (response.status === 201) {
-			// If account was successfully created, additionally log the user in
-			let response = await loginRequest({
-				username: formData.get("username"),
-				password: formData.get("password"),
-				remember: true,
-			});
-
-			if (response.status === 200) {
-				window.location.href =
-					"/account/success?username=" + formData.get("username");
-			} else {
-				clearErrorHighlight();
-				errorAlert(
-					document.querySelector("#error-login-alert"),
-					"Invalid username/password."
-				);
-			}
-		} else {
-			let error = await response.json();
-			// Handle individual errors
-			if (response.status === 400) {
-				clearErrorHighlight();
-				error.forEach((e) => {
-					errorAlert(
-						document.querySelector(`#${e.path[0]}-signup-alert`),
-						"Invalid " + e.path[0] + "."
-					);
-					document
-						.querySelector(`#${e.path[0]}-signup-input`)
-						.classList.add("error-highlight");
-					document
-						.querySelector(`#${e.path[0]}-signup-label`)
-						.classList.add("error-label");
+						if (response.status === 200) {
+							window.location.href =
+								"/account/success?username=" +
+								formData.get("username");
+						} else {
+							clearErrorHighlight();
+							errorAlert(
+								document.querySelector("#error-login-alert"),
+								"Invalid username/password."
+							);
+						}
+					} else {
+						let error = await response.json();
+						// Handle individual errors
+						if (response.status === 400) {
+							clearErrorHighlight();
+							error.forEach((e) => {
+								errorAlert(
+									document.querySelector(
+										`#${e.path[0]}-signup-alert`
+									),
+									"Invalid " + e.path[0] + "."
+								);
+								document
+									.querySelector(`#${e.path[0]}-signup-input`)
+									.classList.add("error-highlight");
+								document
+									.querySelector(`#${e.path[0]}-signup-label`)
+									.classList.add("error-label");
+							});
+						} else {
+							clearErrorHighlight();
+							errorAlert(
+								document.querySelector("#error-signup-alert"),
+								error
+							);
+						}
+					}
 				});
-			} else {
-				clearErrorHighlight();
-				errorAlert(
-					document.querySelector("#error-signup-alert"),
-					error
-				);
-			}
-		}
+		});
 	});
